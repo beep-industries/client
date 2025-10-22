d28fe2ad0f7587795a5c6c7096db392ac690fbb0
Dorian Grasset <doriangrasset@proton.me>
2025-10-22 11:35:48 +0200

    feat: implement theme toggle functionality with dropdown menu

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,14 @@
-import { useState } from 'react'
 import './styles/App.css'
+import './styles/index.css'
+import { ThemeProvider } from './providers/ThemeProvider'
+import { ModeToggle } from '@/features/init/components/ModeToggle'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <ModeToggle />
+    </ThemeProvider>
   )
 }
 
