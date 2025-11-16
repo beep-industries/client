618c6add3fefe1322ed513f38ebc74a7325fa247
pauline <pauline.contat01@etu.umontpellier.fr>
2025-11-16 14:42:58 +0100

    fix

--- src/shared/components/ui/Form.tsx
+++ src/shared/components/ui/Form.tsx
@@ -14,7 +14,7 @@ import {
 } from "react-hook-form"
 
 import { cn } from "@/shared/lib/utils"
-import { Label } from "@/shared/components/ui/label"
+import { Label } from "@/shared/components/ui/Label"
 
 const Form = FormProvider
 
