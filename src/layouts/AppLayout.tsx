8e6777814141dc23b086bb771a32fadd8fb41cf3
Dorian Grasset <doriangrasset@proton.me>
2025-12-11 22:57:47 +0100

    feat: implement NavServer component with server navigation and logout functionality

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,12 +1,14 @@
-import NavServer from "@/components/nav-server"
+import { AppSidebar } from "@/shared/components/AppSidebar"
+import NavServer from "@/shared/components/NavServer"
 import { SidebarInset, SidebarProvider } from "@/shared/components/ui/Sidebar"
 import { Outlet } from "@tanstack/react-router"
 
 export default function AppLayout() {
   return (
-    <div className="flex h-screen w-screen bg-red-500">
+    <div className="flex h-screen w-screen">
       <SidebarProvider>
         {/* LEFT SECTION - Navigation principale */}
+        <AppSidebar />
 
         {/* MIDDLE SECTION - Contenu principal */}
         <SidebarInset className="flex-1 overflow-auto p-6 pt-8">
