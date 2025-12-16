824a15f8786d268e0dce11bccc455b013a7e8271
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-12-16 11:24:46 +0100

    fix: add aria label in button loader

--- src/shared/components/ui/Button.tsx
+++ src/shared/components/ui/Button.tsx
@@ -67,6 +67,7 @@ function Button({
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
+          aria-label="Loading"
         >
           <circle
             className="opacity-25"
