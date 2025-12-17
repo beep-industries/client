8c71429a9b7e7ddedd206c17fad386dac7fd4948
Dorian Grasset <doriangrasset@proton.me>
2025-12-17 17:23:43 +0100

    fix: update component reference in friends route to use FriendsIndexPage

--- src/routes/friends/index.tsx
+++ src/routes/friends/index.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/friends/")({
-  component: RouteComponent,
+  component: FriendsIndexPage,
 })
 
-function RouteComponent() {
+function FriendsIndexPage() {
   return <div>Hello "/friends/"!</div>
 }
