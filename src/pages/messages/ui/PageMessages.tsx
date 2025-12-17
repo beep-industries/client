6a5a80cbd8901c0bb819917d2c4f35286a43b48d
Dorian TETU <doriantetu34@gmail.com>
2025-12-17 13:35:48 +0100

    feat: add message page and sendingbar
    # Conflicts:
    #	pnpm-lock.yaml
    #	src/routes/servers/$id/$channelId/index.tsx

--- src/pages/messages/ui/PageMessages.tsx
+++ src/pages/messages/ui/PageMessages.tsx
@@ -1,3 +1,14 @@
+import SendingBar from "@/shared/components/SendingBar"
+import TopBar from "@/shared/components/TopBar"
+
 export default function PageMessages() {
-  return <div></div>
+  return (
+    <div className="flex h-screen flex-col justify-between">
+      <TopBar />
+      <div id="messages-section" className="h-full overflow-scroll"></div>
+      <div className="flex justify-center p-4">
+        <SendingBar />
+      </div>
+    </div>
+  )
 }
