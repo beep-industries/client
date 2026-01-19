8b1e12d9fbde380a01f9d786e2312ccdf6628f18
Dorian TETU <doriantetu34@gmail.com>
2026-01-19 08:51:54 +0100

    fix: rebasing with main and fixing ui

--- src/pages/messages/ui/PageMessages.tsx
+++ src/pages/messages/ui/PageMessages.tsx
@@ -1,10 +1,8 @@
 import SendingBar from "@/shared/components/SendingBar"
-import TopBar from "@/shared/components/TopBar"
 
 export default function PageMessages() {
   return (
-    <div className="flex h-screen flex-col justify-between">
-      <TopBar />
+    <div className="flex h-full flex-col justify-between">
       <div id="messages-section" className="h-full overflow-scroll"></div>
       <div className="flex justify-center p-4">
         <SendingBar />
