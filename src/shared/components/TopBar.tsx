6151d3b9e651d33e913a562008ee9d9f1d5b0a2b
Dorian Grasset <doriangrasset@proton.me>
2026-01-26 18:32:34 +0100

    refactor: comment out unused imports and translation in TopBar component for futur use

--- src/shared/components/TopBar.tsx
+++ src/shared/components/TopBar.tsx
@@ -1,7 +1,7 @@
-import { Bell } from "lucide-react"
-import { useTranslation } from "react-i18next"
-import { Button } from "./ui/Button"
-import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/Tooltip"
+// import { Bell } from "lucide-react"
+// import { useTranslation } from "react-i18next"
+// import { Button } from "./ui/Button"
+// import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/Tooltip"
 import SidebarTrigger from "./SidebarTrigger"
 
 interface TopBarProps {
@@ -9,13 +9,13 @@ interface TopBarProps {
 }
 
 export default function TopBar({ children }: TopBarProps) {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
 
   return (
     <div className="bg-sidebar border-sidebar-border flex flex-row items-center justify-between gap-2 border-b p-2">
       <SidebarTrigger />
       {children}
-      <Tooltip>
+      {/* <Tooltip>
         <TooltipTrigger asChild>
           <Button variant="ghost" size="icon" className="size-7">
             <Bell />
@@ -23,7 +23,7 @@ export default function TopBar({ children }: TopBarProps) {
           </Button>
         </TooltipTrigger>
         <TooltipContent>{t("topBar.notifications")}</TooltipContent>
-      </Tooltip>
+      </Tooltip> */}
     </div>
   )
 }
